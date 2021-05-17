7c358f567d7b9e2f8f68ef1ec303352f2b8ddbf2
Dantum <dandeltumaag@github.com>
2021-05-17 10:35:44 +0800

    ADD: Redux for CARS

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade'
 
 function Section({title, description, backgroundImg, leftBtnText, rightBtnText}) {
 	return (
 		<Wrap bgImage={backgroundImg}> {/* pass props */}
-			<ItemText>
-				<h1>{title}</h1>
-				<p>{description}</p>
-			</ItemText>
+			<Fade bottom>
+				<ItemText>
+					<h1>{title}</h1>
+					<p>{description}</p>
+				</ItemText>
+			</Fade>
 			<Buttons>
-				<ButtonGroup>
-					<LeftButton>
-						{leftBtnText}
-					</LeftButton>
-					{rightBtnText && 
-						<RightButton>
-							{rightBtnText}
-						</RightButton>
-					} 
-				</ButtonGroup>
+				<Fade bottom>
+					<ButtonGroup>
+						<LeftButton>
+							{leftBtnText}
+						</LeftButton>
+						{rightBtnText && 
+							<RightButton>
+								{rightBtnText}
+							</RightButton>
+						} 
+					</ButtonGroup>
+				</Fade>
 				<DownArrow src="/images/down-arrow.svg" />
 			</Buttons>
 		</Wrap>
@@ -40,7 +45,6 @@ const Wrap = styled.div `
 	align-items: center; /* vertical alignment */
 
 	background-image: ${props => `url("/images/${props.bgImage}")`};
-
 `
 
 const ItemText = styled.div `
